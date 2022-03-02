989c4cc2963d77107cf56929e511cf18bed3cf29
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-03-01 17:22:23 -0800

    add all-stratum sim and mute message for dplyr messages

--- R/bootstrap_goa.R
+++ R/bootstrap_goa.R
@@ -2,7 +2,7 @@
 # POP
 source("R/01_cleanup_data.R") #from design-based code
 library(tidyverse)
-
+options(dplyr.summarise.inform = FALSE)
 
 
 # Preliminary stuff -------------------------------------------------------
@@ -84,7 +84,7 @@ boots_biomass<- vector()
 set.seed(123)
 
 for(i in 1:nboots){
-x <- sample(1:nrow(abund_hauls), size = samplesize)
+x <- ifelse(samplesize==0,1,sample(1:nrow(abund_hauls), size = samplesize))
 boot_hauls <- abund_hauls[x,]
 boot_cpue <- get_cpue(racebase_tables = list(
   cruisedat = cruisedat,
@@ -100,16 +100,18 @@ boots_biomass[i] <- boot_biomass_stratum$stratum_biomass
 
 png("img/POP_GOA_2021_1000.png",width = 10,height = 5,units='in',res = 120)
 par(mfrow=c(1,2))
-hist(log(boots_biomass),freq=FALSE,main = "POP - GOA - 2021",xlab="Log(stratum biomass)")
+hist(log(boots_biomass),freq=FALSE,main = "POP - GOA - 2021 - stratum 130",xlab="Log(stratum biomass)",xlim=c(min(log(boots_biomass)),max(log(boots_biomass)*1.05)))
 curve(dnorm(x, mean=mean(log(boots_biomass)), sd=sd(log(boots_biomass))), add=TRUE,
       from=min(log(boots_biomass)), to=max(log(boots_biomass)),
       lwd=2)
 
-qqnorm(log(boots_biomass),main = "POP - GOA - 2021")
+qqnorm(log(boots_biomass),main = "POP - GOA - 2021 - stratum 130")
 qqline(log(boots_biomass), col = 2)
 dev.off()
 
-GOAPOP_2021 <- boots_biomass
+bb <- data.frame(sim=1:length(boots_biomass),stratum = 130, year = 2021, species = "POP",biomass_stratum_boot=boots_biomass)
+
+GOAPOP_2021 <- bb
 
 
 # Try with arrowtooth -----------------------------------------------------
@@ -119,7 +121,7 @@ boots_biomass<- vector()
 set.seed(123)
 
 for(i in 1:nboots){
-  x <- sample(1:nrow(abund_hauls), size = samplesize)
+  x <- ifelse(samplesize==0,1,sample(1:nrow(abund_hauls), size = samplesize))
   boot_hauls <- abund_hauls[x,]
   boot_cpue <- get_cpue(racebase_tables = list(
     cruisedat = cruisedat,
@@ -135,11 +137,74 @@ for(i in 1:nboots){
 
 png("img/ATF_GOA_2021_1000.png",width = 10,height = 5,units='in',res = 120)
 par(mfrow=c(1,2))
-hist(log(boots_biomass),freq=FALSE,main = "ATF - GOA - 2021",xlab="Log(stratum biomass)")
+hist(log(boots_biomass),freq=FALSE,main = "ATF - GOA - 2021",xlab="Log(stratum biomass)",xlim=c(min(log(boots_biomass)),max(log(boots_biomass)*1.05)))
 curve(dnorm(x, mean=mean(log(boots_biomass)), sd=sd(log(boots_biomass))), add=TRUE,
-      from=min(log(boots_biomass)), to=max(log(boots_biomass)),
+      from=min(log(boots_biomass)), to=max(log(boots_biomass)*1.05),
       lwd=2)
 
 qqnorm(log(boots_biomass),main = "ATF - GOA - 2021")
 qqline(log(boots_biomass), col = 2)
 dev.off()
+
+bb <- data.frame(sim=1:length(boots_biomass),stratum = 130, year = 2021, species = "ATF",biomass_stratum_boot=boots_biomass)
+GOAATF_2021 <- bb
+
+save(file = 'outputs/POP_ATF.Rdata',list = c("GOAATF_2021","GOAPOP_2021"))
+
+
+# Big simulation with multiple strata -------------------------------------
+strata_boot <- unique(haul$stratum) #, 221, 250, 131 eyeballed these to see which strata have similar biomasses
+nboots <- 1000  
+
+POP_out <- vector()
+for (j in 1:length(strata_boot)){
+  abund_hauls <- haul %>% 
+    filter(abundance_haul=="Y" & 
+             lubridate::year(start_time) == 2021 & 
+             stratum %in% strata_boot[j] & 
+             region == "GOA")
+  if(nrow(abund_hauls)==0){next}
+  
+  # Size of bootstrap sample - 50% of total hauls?
+  samplesize <- floor(nrow(abund_hauls) * .5)
+  
+  boots_biomass<- vector()
+  set.seed(123)
+
+for(i in 1:nboots){
+  x <- ifelse(samplesize==0,1,sample(1:nrow(abund_hauls), size = samplesize))
+  boot_hauls <- abund_hauls[x,]
+  boot_cpue <- get_cpue(racebase_tables = list(
+    cruisedat = cruisedat,
+    haul = boot_hauls, #***
+    catch = catch
+  ), speciescode = 30060,
+  survey_area = "GOA")
+  boot_biomass_stratum <- get_biomass_stratum(cpue_table = boot_cpue, 
+                                              speciescode = 30060, 
+                                              survey_area = "GOA")
+  boots_biomass[i] <- boot_biomass_stratum$stratum_biomass
+}
+
+bb <- data.frame(sim=1:length(boots_biomass),stratum = strata_boot[j], year = 2021, species = "POP",biomass_stratum_boot=boots_biomass)
+POP_out <- rbind(POP_out,bb)
+cat(j/length(strata_boot))
+}
+
+save(list = "POP_out",file = "output/POP_allstrata.Rdata")
+
+# png("img/ATF_GOA_2021_1000.png",width = 10,height = 5,units='in',res = 120)
+# par(mfrow=c(1,2))
+# hist(log(boots_biomass),freq=FALSE,main = "ATF - GOA - 2021",xlab="Log(stratum biomass)",xlim=c(min(log(boots_biomass)),max(log(boots_biomass)*1.05)))
+# curve(dnorm(x, mean=mean(log(boots_biomass)), sd=sd(log(boots_biomass))), add=TRUE,
+#       from=min(log(boots_biomass)), to=max(log(boots_biomass)*1.05),
+#       lwd=2)
+# 
+# qqnorm(log(boots_biomass),main = "ATF - GOA - 2021")
+# qqline(log(boots_biomass), col = 2)
+# dev.off()
+
+
+
+#save(file = 'outputs/POP_ATF.Rdata',list = c("GOAATF_2021","GOAPOP_2021"))
+
