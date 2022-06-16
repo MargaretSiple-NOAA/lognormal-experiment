6598d51d81c733514804b61081878c4c1da1d1a0
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-06-16 12:51:41 -0700

    update biomass_stratum calculation

--- R/03_get_biomass_stratum_cpuein.R
+++ R/03_get_biomass_stratum_cpuein.R
@@ -22,37 +22,32 @@ get_biomass_stratum <- function(cpue_table,
                                   "AI" = ai_strata
                                 )) {
   At <- sum(strata$area)
-
-  # Get cpue table
-  # cpue_table <- get_cpue(
-  #   racebase_tables = racebase_tables,
-  #   survey_area = survey_area,
-  #   speciescode = speciescode
-  # )
+  
   # Total CPUE for species, year, stratum
   # no RACEBASE equivalent (building block of BIOMASS_STRATUM)
   x2 <- cpue_table %>%
     group_by(year, stratum) %>%
     dplyr::summarize(
-      haul_count = length(unique(stationid)), # number of total abundance hauls
-      mean_wgt_cpue = mean(WGTCPUE,na.rm=TRUE),
-     # var_wgt_cpue = ifelse(haul_count <= 1, NA, var(WGTCPUE) / haul_count),
-     # mean_num_cpue = mean(NUMCPUE,na.rm=TRUE),
-    #  var_num_cpue = ifelse(haul_count <= 1, NA, var(NUMCPUE) / haul_count),
-      catch_count = length(which(Catch_KG > 0)) # number of hauls with nonzero catch
+      haul_count = length(unique(hauljoin)), # number of total abundance hauls
+      mean_wgt_cpue = mean(wgtcpue, na.rm = TRUE),
+      var_wgt_cpue = ifelse(haul_count <= 1, NA, var(wgtcpue) / haul_count),
+      mean_num_cpue = mean(numcpue, na.rm = TRUE),
+      var_num_cpue = ifelse(haul_count <= 1, NA, var(numcpue) / haul_count),
+      catch_count = length(which(catch_kg > 0)) # number of hauls with nonzero catch
     ) %>%
     dplyr::ungroup() %>%
     select(
-      year, stratum,
+      year, stratum, 
       haul_count, catch_count,
-      mean_wgt_cpue#, var_wgt_cpue,
-     
-    )
-
+      mean_wgt_cpue, var_wgt_cpue,
+      mean_num_cpue, var_num_cpue
+    ) %>%
+    add_column(.after = "stratum", species_code = speciescode)
+  
   # if (all(x2$catch_count <= x2$haul_count)) {
   #   print("Number of hauls with positive catches is realistic.")
   # }
-
+  
   # RACEBASE equivalent table: BIOMASS_STRATUM
   biomass_stratum <- x2 %>%
     dplyr::left_join(strata, by = "stratum") %>%
@@ -60,22 +55,22 @@ get_biomass_stratum <- function(cpue_table,
     mutate(
       stratum_biomass = area * mean_wgt_cpue / vulnerability * 0.001, # kg --> mt
       stratum_ratio = area / At,
-    #  biomass_var = area^2 * var_wgt_cpue * 1e-6, # kg--> mt, square it because it's variance
-     # qt_size = ifelse(haul_count <= 1, 0,
-    #    qt(p = 0.025, df = haul_count - 1, lower.tail = F)
-    #  ),
-     # min_biomass = stratum_biomass - qt_size * sqrt(biomass_var),
-     # max_biomass = stratum_biomass + qt_size * sqrt(biomass_var),
-    #  stratum_pop = area * mean_num_cpue, # not sure why this calculation would be different from the stratum_biomass
-    #  pop_var = area^2 * var_num_cpue,
-    #  min_pop = stratum_pop - qt_size * sqrt(pop_var),
-    #  max_pop = stratum_pop + qt_size * sqrt(pop_var)
+      biomass_var = area^2 * var_wgt_cpue * 1e-6, # kg--> mt, square it because it's variance
+      qt_size = ifelse(haul_count <= 1, 0,
+                       qt(p = 0.025, df = haul_count - 1, lower.tail = F)
+      ),
+      min_biomass = stratum_biomass - qt_size * sqrt(biomass_var),
+      max_biomass = stratum_biomass + qt_size * sqrt(biomass_var),
+      stratum_pop = area * mean_num_cpue, # not sure why this calculation would be different from the stratum_biomass
+      pop_var = area^2 * var_num_cpue,
+      min_pop = stratum_pop - qt_size * sqrt(pop_var),
+      max_pop = stratum_pop + qt_size * sqrt(pop_var)
     ) %>%
-   # mutate(
-  #    min_biomass = ifelse(min_biomass < 0, 0, min_biomass),
-  #    min_pop = ifelse(min_pop < 0, 0, min_pop)
-  #  ) %>% # set low CI to zero if it's negative
-    select(survey, year, stratum, stratum_ratio, haul_count, mean_wgt_cpue,stratum_biomass, area) %>%
+    mutate(
+      min_biomass = ifelse(min_biomass < 0, 0, min_biomass),
+      min_pop = ifelse(min_pop < 0, 0, min_pop)
+    ) %>% # set low CI to zero if it's negative
+    select(survey, year, stratum, species_code, haul_count, catch_count, mean_wgt_cpue, var_wgt_cpue, mean_num_cpue, var_num_cpue, stratum_biomass, biomass_var, min_biomass, max_biomass, stratum_pop, pop_var, min_pop, max_pop, area, stratum_ratio) %>%
     mutate(
       Ni = area / 0.01,
       fi = (Ni * (Ni - haul_count)) / haul_count
