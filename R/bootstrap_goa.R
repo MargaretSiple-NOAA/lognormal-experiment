2c5fb9ffc16391ab4d12eaf06d8334ddaf8d0d33
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-06-16 12:52:45 -0700

    add skeleton for faster bootstrap list

--- R/bootstrap_goa.R
+++ R/bootstrap_goa.R
@@ -2,8 +2,11 @@
 # POP
 source("R/01_cleanup_data.R") # from design-based code
 library(tidyverse)
+
 options(dplyr.summarise.inform = FALSE)
 
+# Folder to store outputs
+dir.create(paste0("outputs/",Sys.Date()))
 
 # Preliminary stuff -------------------------------------------------------
 # Look at strata, see which are similar and diff in terms of biomass mean and CV
@@ -16,6 +19,7 @@ samplecounts <- haul %>%
   group_by(cruise, stratum) %>%
   count() %>%
   ungroup()
+
 
 samplecounts %>% filter(cruise == 202101)
 
@@ -47,60 +51,71 @@ rownames(xx) <- colnames(xx) <- x$stratum
 xx[lower.tri(xx)] <- NA # get upper triangle only
 xx <- abs(xx)
 
-# xx %>%
-#   as_tibble() %>%
-#   pivot_longer(cols=colnames(xx), names_to = "stratum", values_to = "biomass_diff") %>%
-#   filter(value !=0) %>%
-#   top_n(wt = value, n = -10)
 
-
-# Example with POP --------------------------------------------------------
+#Example --------------------------------------------------------
 # These scripts are all from design-based-indices (a repo which should be a package)
 
 source("R/03_get_biomass_stratum_cpuein.R")
 source("R/04_get_biomass_total_stratumin.R")
 
 
-#####
+
+# Set params for the bootstrapping ----------------------------------------
+species_in <- 30060
+yr_in <- 2021
+region_in <- "GOA"
+nboots <- 20
+# all strata will be included
 
 # Bootstrap at haul level -------------------------------------------------
 # Main question: are stratum-level biomass estimates actually lognormally distributed? Bootstrap at haul scale within a stratum (?) to find out.
-# subset to abundance hauls in GOA 2021
 
-# Size of bootstrap sample and which stratum to focus on
-strata_boot <- c(130) # , 221, 250, 131 eyeballed these to see which strata have similar biomasses
-nboots <- 1000
-
+# Prep haul data
 abund_hauls <- haul %>%
   filter(abundance_haul == "Y" &
     lubridate::year(start_time) == 2021 &
-    stratum %in% strata_boot &
     region == "GOA")
 
-# Size of bootstrap sample - 50% of total hauls?
-samplesize <- floor(nrow(abund_hauls) * .5)
 
-boots_biomass <- vector()
-set.seed(123)
+# Set up dataframe with bootstrap design ----------------------------------
+# Size of bootstrap sample - 50% of total hauls
+prop_boot <- 0.5
+#(samplesize <- floor(nrow(abund_hauls) * prop_boot))
+
+
+# Generate bootstrapped CPUE ----------------------------------------------
+bootslist <- list()
+
+start.time <- Sys.time() # 13 sec for 1000 boots, 1.4 mins for the whole thing+cpue tables
 
 for (i in 1:nboots) {
-  x <- ifelse(samplesize == 0, 1, sample(1:nrow(abund_hauls), size = samplesize, replace = TRUE))
-  boot_hauls <- abund_hauls[x, ]
-  boot_cpue <- get_cpue(
+  boothaul <- abund_hauls %>%
+    group_by(stratum) %>%
+    dplyr::slice_sample(prop = prop_boot, replace = TRUE) %>%
+    ungroup()
+  
+  bootslist[[i]] <- get_cpue(
     racebase_tables = list(
       cruisedat = cruisedat,
-      haul = boot_hauls, #***
+      haul = boothaul, #***
       catch = catch
-    ), speciescode = 30060,
-    survey_area = "GOA"
-  )
-  boot_biomass_stratum <- get_biomass_stratum(
-    cpue_table = boot_cpue,
-    speciescode = 30060,
-    survey_area = "GOA"
-  )
-  boots_biomass[i] <- boot_biomass_stratum$stratum_biomass
+    ), speciescode = species_in,
+    survey_area = region_in
+  ) %>%
+    mutate(boot_id = i)
 }
+
+Sys.time() - start.time
+
+# Save list of 
+save(bootslist,file = paste0("outputs/",Sys.Date(),"/","CPUE_",species_in,"_",yr_in,"_",region_in,".RData"))
+
+
+
+
+
+
+
 
 png("img/POP_GOA_2021_1000.png", width = 10, height = 5, units = "in", res = 120)
 par(mfrow = c(1, 2))
