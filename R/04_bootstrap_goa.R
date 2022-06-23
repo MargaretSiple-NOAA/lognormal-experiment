4feef975c744f6667e729a42cc2db9e8a80fed42
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-06-23 13:46:03 -0700

    add a big loop with safe species 2007-2021

--- R/04_bootstrap_goa.R
+++ R/04_bootstrap_goa.R
@@ -35,7 +35,7 @@ summary(samplecounts)
 source("R/02_get_cpue.R")
 # source("R/03_get_biomass_stratum.R")
 source("R/03_get_biomass_stratum_cpuein.R")
-source("R/04_get_biomass_total.R")
+#source("R/04_get_biomass_total.R")
 
 # Set up species, year, region for the bootstrapping ----------------------
 species_in <- 10110 # Set species. POP = 30060, ATF = 10110
@@ -127,3 +127,77 @@ test %>%
   ) +
   facet_wrap(~stratum, scales = "free_y")
 
+
+# Bootstraps for all the SAFE species in GOA ------------------------------
+
+safe <- read.csv("data/siple_safe_species.csv") %>%
+  filter(!is.na(species_code)) %>%
+  select(spp_name_informal, GOA, species_code)
+yrs <- seq(2007, 2021, by = 2)
+
+for (s in 1:length(unique(safe$species_code))) {
+  for (y in 1:length(yrs)) {
+    species_in <- safe$species_code[s] # Set species. POP = 30060, ATF = 10110
+    yr_in <- yrs[y]
+    region_in <- "GOA"
+    nboots <- 1000
+    # no need to specify stratum
+
+    # Bootstrap at haul level -------------------------------------------------
+    # Main question: are stratum-level biomass estimates actually lognormally distributed? Bootstrap at haul scale within a stratum (?) to find out.
+
+    # Prep haul data
+    abund_hauls <- haul %>%
+      filter(abundance_haul == "Y" &
+        lubridate::year(start_time) == yr_in &
+        region == region_in)
+
+
+    # Set up dataframe with bootstrap design ----------------------------------
+    # Size of bootstrap sample - 50% of total hauls
+    prop_boot <- 1
+    # (samplesize <- floor(nrow(abund_hauls) * prop_boot))
+
+
+    # Generate bootstrapped CPUE ----------------------------------------------
+    bootscpue <- list()
+
+    start.time <- Sys.time() # 13 sec for 1000 boots, 1.4 mins for the whole thing+cpue tables
+
+    for (i in 1:nboots) {
+      boothaul <- abund_hauls %>%
+        group_by(stratum) %>%
+        dplyr::slice_sample(prop = prop_boot, replace = TRUE) %>%
+        ungroup()
+
+      bootscpue[[i]] <- get_cpue(
+        racebase_tables = list(
+          cruisedat = cruisedat,
+          haul = boothaul,
+          catch = catch
+        ), speciescode = species_in,
+        survey_area = region_in
+      ) %>%
+        mutate(boot_id = i)
+    } #/end boots
+
+    Sys.time() - start.time
+
+    save(bootscpue,
+      file = paste0("outputs/", Sys.Date(), "/", "CPUE_", species_in, "_", yr_in, "_", region_in, ".RData")
+    )
+
+    bootsbiomassstratum <- lapply(bootscpue,
+      FUN = function(x) {
+        get_biomass_stratum(
+          cpue_table = x,
+          speciescode = species_in,
+          survey_area = region_in
+        )
+      }
+    )
+
+    save(bootsbiomassstratum, file = paste0("outputs/", Sys.Date(), "/", "stratumbiomass_", species_in, "_", yr_in, "_", region_in, ".RData"))
+  }
+  print(s/length(unique(safe$species_code)))
+}
