8545153e95ea9357ed5345d226a40e8cbe340d60
mcsiple-NOAA <margaret.siple@noaa.gov>
2022-06-27 11:51:08 -0800

    cleanup

--- R/xx_get_foss_cpue.R
+++ R/xx_get_foss_cpue.R
@@ -5,15 +5,11 @@ library("jsonlite")
 api_link <- "https://origin-tst-ods-st.fisheries.noaa.gov/ods/foss/afsc_groundfish_survey/"
 
 res <- httr::GET(api_link)
-# base::rawToChar(res$content) # Test connection
 data <- jsonlite::fromJSON(base::rawToChar(res$content))
-
 head(data)
 
-
 # Try just for GOA species ------------------------------------------------
-
-res <- httr::GET(api_link, query = list(srvy = "EBS", year = 2017))
+res <- httr::GET(api_link, query = list(srvy = "GOA", year = 2017))
 data <- jsonlite::fromJSON(base::rawToChar(res$content))
 x <- data$items
 head(x)
@@ -22,8 +18,9 @@ head(x)
 res <- httr::GET(api_link, query = list(srvy = "EBS", year = 2018))
 data <- jsonlite::fromJSON(base::rawToChar(res$content))
 x <- data$items
-x <- x[,c("stratum", "station", "vessel_name", "latitude_dd", "longitude_dd", 
-          "species_code", "common_name", "scientific_name", "taxon_confidence", 
-          "cpue_kgha", "cpue_noha", "weight_kg", "count", 
-          "bottom_temperature_c", "surface_temperature_c", "depth_m")]
+head(x)
+# x <- x[,c("stratum", "station", "vessel_name", "latitude_dd", "longitude_dd", 
+#           "species_code", "common_name", "scientific_name", "taxon_confidence", 
+#           "cpue_kgha", "cpue_noha", "weight_kg", "count", 
+#           "bottom_temperature_c", "surface_temperature_c", "depth_m")]
 head(x)
